a3d3b7bf5f6473bd9d6b022afb5f15f6e2c308c5
Lena Kujath <l.kujath@gmx.de>
2020-06-09 23:04:08 +0200

    updated register form

--- src/Components/Register/User/UserForm/Signup.js
+++ src/Components/Register/User/UserForm/Signup.js
@@ -7,13 +7,11 @@ const SignUp = ({ language }) => {
 
     // const { logUserIntoContext } = React.useContext(MyContext);
     // const [users, updateUsers] = useState([]);
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+  
     const [username, setUsername] = useState('');
-    const [birthdate, setBirthDate] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [repeatedPassword, setRepeatedPassword] = useState('');
+    // const [repeatedPassword, setRepeatedPassword] = useState('');
     const [posted, setPosted] = useState(false);
 
     // State where we are going to keep the error and messages on the register page that we receive from the backend
@@ -52,23 +50,6 @@ const SignUp = ({ language }) => {
     return (
         <div id="signup-tab-content" className="active tabs-content">
             <form className="signup-form" onSubmit={postProfile}>
-                <input
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    name="first_name"
-                    className="input"
-                    placeholder='First name'
-                    // placeholder={texts[language].firstName}
-                />
-                <br />
-                <input
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    name="last_name"
-                    className="input"
-                    placeholder='Last name'
-                    // placeholder={texts[language].lastName}
-                />
                 <br />
                 <input
                     value={username}
@@ -79,15 +60,6 @@ const SignUp = ({ language }) => {
                     type="text"
                     className="input"
                     id="user_name"
-                />
-                <br />
-                <input
-                    value={birthdate}
-                    onChange={(e) => setBirthDate(e.target.value)}
-                    name="birth_date"
-                    className="input"
-                    placeholder="Birth date DD/MM/YYYY"
-                    // placeholde//r={texts[language].birth_date + " (D/M/Y)"}
                 />
                 <br />
                 <input
@@ -110,16 +82,6 @@ const SignUp = ({ language }) => {
                     type="password"
                     className="input"
                     id="user_pass"
-                />
-                <input
-                    value={repeatedPassword}
-                    onChange={(e) => setRepeatedPassword(e.target.value)}
-                    name="repeatPassword"
-                    placeholder='Repeat password'
-                    // placeholder={texts[language].repeatPassword}
-                    type="password"
-                    className="input"
-                    id="user_repeat_pass"
                 />
                 <br />
                 <button className="form-btn" type="submit">
